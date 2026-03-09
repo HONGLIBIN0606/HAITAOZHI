789266c66a5447ed887c070d11fb92087081f492
阿星不是程序员 <1031900093@qq.com>
2026-03-09 09:57:00 +0800

    修改 Kafka 消费的说明

--- hmdp-core-service/src/main/java/org/javaup/kafka/consumer/SeckillVoucherConsumer.java
+++ hmdp-core-service/src/main/java/org/javaup/kafka/consumer/SeckillVoucherConsumer.java
@@ -43,11 +43,6 @@ import static org.javaup.constant.Constant.SPRING_INJECT_PREFIX_DISTINCTION_NAME
 /**
  * @program: 黑马点评-plus升级版实战项目。添加 阿星不是程序员 微信，添加时备注 点评 来获取项目的完整资料
  * @description: Kafka 消费者：处理秒杀券下单消息。
- * 负责：
- * 1) 延迟过滤：超过阈值的消息丢弃并回滚；
- * 2) 正常消费：创建订单，幂等冲突时执行回滚；
- * 3) 失败处理：消费异常时回滚并记录对账日志；
- * 4) 成功处理：消费成功记录一致性对账日志。
  * @author: 阿星不是程序员
  **/
 
